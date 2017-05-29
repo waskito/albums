7aadc5c874e967b0ab15c6264f85acf1a4182a1a
Yanuar Waskito <yg.waskito@gmail.com>
2017-05-29 10:31:10 +0700

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {
+  Text,
+  AppRegistry
+} from 'react-native';
+
+const App = () => (
+  <Text>Some Text</Text>
+);
+
+AppRegistry.registerComponent('albums', () => App);
